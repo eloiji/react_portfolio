b96a282828c0bf834fbce21cfb647b11cdc764ac
eloiji <eloisajocsoniting@gmail.com>
2025-09-30 23:32:59 +0800

    task: add scroll animation for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,31 @@ const socials = [
 ];
 
 const Header = () => {
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const headerElement = headerRef.current;
+      if (!headerElement) {
+        return;
+      }
+      if (prevScrollPos > currentScrollPos) {
+        headerElement.style.transform = "translateY(0)";
+      } else {
+        headerElement.style.transform = "translateY(-200px)";
+      }
+      prevScrollPos = currentScrollPos;
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
@@ -56,6 +81,7 @@ const Header = () => {
       transitionTimingFunction="ease-in-out"
       backgroundColor="#18181b"
       zIndex={10}
+      ref={headerRef} 
     >
       <Box color="white" maxWidth="1280px" margin="0 auto">
         <HStack
